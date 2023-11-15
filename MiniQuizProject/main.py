d1e039801840310e80f7cbe4e05a0b7ab6c37471
DayDreamY <53157668+DayDreamYGithub@users.noreply.github.com>
2023-11-15 21:16:16 +0900

    Add files via upload

---
+++ MiniQuizProject/main.py
@@ -0,0 +1,20 @@
+from question_model import Quesion
+from data import question_data
+from quiz_brain import Quiz_brain
+
+question_bank=[]
+for question in question_data:
+    question_text=question["text"]
+    question_answer=question["answer"]
+    new_question=Quesion(question_text,question_answer)
+    question_bank.append((new_question))
+
+quiz=Quiz_brain(question_bank)
+quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You are done!")
+print("You final score is : " + str(quiz.score) + "!")
+
